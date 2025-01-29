c7a85c855f469fcd02b4e558986c909033c9043b
Hari <harikrushnasakhiya9@gmail.com>
2025-01-29 20:56:39 +0530

    api host live

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ dotenv.config()
 import express from 'express'
 import cors from 'cors'
 import connectDB from './config/connectdb.js'
-import UserRoutes from './routes/userROuter.js'
+import UserRoutes from './routes/userRouter.js'
 
 
 const app = express()
